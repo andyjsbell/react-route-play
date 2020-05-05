97f40612a786bb1dd856190893be3ca68a5fdd21
Andy Bell <andy.bell@displaynote.com>
2020-05-05 17:35:55 +0200

    parse JSON back and forth to local storage

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { BrowserRouter, Switch, Link, Route, Redirect, useLocation, useHistory, 
 
 const useStateWithLocalStorage = (localStorageKey) => {
   const [value, setValue] = React.useState(
-    localStorage.getItem(localStorageKey) || ''
+    JSON.parse(localStorage.getItem(localStorageKey))
   );
 
   React.useEffect(() => {
-    localStorage.setItem(localStorageKey, value);
+    localStorage.setItem(localStorageKey, JSON.stringify(value));
   }, [value]);
 
   return [value, setValue];
